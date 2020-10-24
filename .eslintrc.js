bc3d22580f2f467c7ffb32f4399eee9873498a84
Anastasia Bykadorova <nastia.byckadorova@gmail.com>
2020-10-24 22:57:34 +0300

    add eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,7 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
@@ -14,5 +11,176 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        'singleline': 1,
+        'multiline': {
+          'max': 1,
+          'allowFirstLine': false
+        }
+      }
+    ],
+    'vue/attributes-order': [
+      2,
+      {
+        'order': [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          'UNIQUE',
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT'
+        ],
+        'alphabetical': false
+      }
+    ],
+    'vue/multiline-html-element-content-newline': 1,
+    'import/prefer-default-export': 0,
+    'import/no-named-as-default': 0, // disabled to for constants/timeUnitStatuses. It has named format export and format function
+    'import/no-named-as-default-member': 0, // disabled to for DateHelper. It has named format export and format function
+    'import/no-extraneous-dependencies': 0,
+    'import/order': [
+      2,
+      {
+        'newlines-between': 'always',
+        'pathGroups': [
+          {
+            'pattern': '@/**',
+            'group': 'parent',
+            'position': 'before'
+          }
+        ],
+        'alphabetize': {
+          'order': 'asc',
+          'caseInsensitive': true
+        },
+        'pathGroupsExcludedImportTypes': [
+          'builtin'
+        ]
+      }
+    ],
+    'no-console': 2,
+    'no-var': 2,
+    'no-new': 0,
+    'no-useless-constructor': 0,
+    'no-return-assign': 0,
+    'no-duplicate-imports': 2,
+    'object-curly-newline': [
+      1,
+      {
+        'consistent': true
+      }
+    ],
+    'linebreak-style': 0,
+    'lines-between-class-members': 0,
+    'camelcase': 0,
+    'semi': [
+      2,
+      'always'
+    ],
+    'comma-dangle': [
+      2,
+      {
+        'arrays': 'always-multiline',
+        'objects': 'always-multiline',
+        'imports': 'always-multiline',
+        'exports': 'always-multiline',
+        'functions': 'only-multiline' // 'always-' conflicts with multiline webpack dynamic imports
+      }
+    ],
+    'no-multiple-empty-lines': [
+      1,
+      {
+        'max': 1
+      }
+    ],
+    'eol-last': [
+      1,
+      'always'
+    ],
+    'padding-line-between-statements': [
+      1,
+      {
+        'blankLine': 'always',
+        'prev': [
+          'const',
+          'let',
+          'var'
+        ],
+        'next': '*'
+      },
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': [
+          'if',
+          'try',
+          'class',
+          'export'
+        ]
+      },
+      {
+        'blankLine': 'always',
+        'prev': [
+          'if',
+          'try',
+          'class',
+          'export'
+        ],
+        'next': '*'
+      },
+      {
+        'blankLine': 'any',
+        'prev': [
+          'const',
+          'let',
+          'var',
+          'export'
+        ],
+        'next': [
+          'const',
+          'let',
+          'var',
+          'export'
+        ]
+      }
+    ],
+    'block-spacing': 1,
+    'comma-spacing': [
+      1,
+      {
+        'before': false,
+        'after': true
+      }
+    ],
+    'func-call-spacing': [
+      1,
+      'never'
+    ],
+    'newline-before-return': 1,
+    'object-curly-spacing': [
+      1,
+      'always',
+      {
+        'arraysInObjects': true,
+        'objectsInObjects': true
+      }
+    ],
+    'padded-blocks': [
+      1,
+      'never'
+    ],
+    'max-len': [
+      1,
+      {
+        'code': 120
+      }
+    ]
   },
 };
